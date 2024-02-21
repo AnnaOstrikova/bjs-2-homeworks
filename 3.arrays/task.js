23960f8085fd86703879343deba9016dca8f10c1
AnnaOstrikova <pomozovaanna@yandex.ru>
2024-02-21 13:53:44 +0500

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,38 @@
-function compareArrays(arr1, arr2) {
-  
+function compareArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+    }else{
+    return array1.every(function(element, index) {
+      return element === array2[index];
+    });
+  }
 }
-
+ 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (gender === "женский"){
+      let womenUsers = users.filter(user => user.gender === "женский");
+      let totalAgeWomen =womenUsers.map(womenUser => womenUser.age);
+      let averageAgeWomen = totalAgeWomen.reduce((acc, item, index, arr) => {
+          acc += item;
+          if(index === arr.length - 1){
+            return acc/arr.length;
+          }
+          return acc;
+      },0);
+      return averageAgeWomen;
+    }
+    if (gender === "мужской"){
+      let menUsers = users.filter(user => user.gender === "мужской");
+      let totalAgeMen =menUsers.map(menUser => menUser.age);
+      let averageAgeMen = totalAgeMen.reduce((acc, item, index, arr) => {
+        acc += item;
+        if(index === arr.length - 1){
+          return acc/arr.length;
+        }
+        return acc;
+      },0);
+      return averageAgeMen;
+    }else{
+      return 0;
+    }   
 }
